337755efd969da5f91a06e20d70d8005a7a9a39f
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-07-31 18:04:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# myMovies-client_angular
+ Client side for myMovies App built with Angular
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// simplified API for Angular applications that makes it possible for the client app to communicate with the API or server-side
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
