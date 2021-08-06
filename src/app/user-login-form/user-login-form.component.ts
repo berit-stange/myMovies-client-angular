e6e97f0cf3a45c7c57d379b9ae4d79a82a4a5bbd
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-08-06 14:36:17 +0200

    added localStorage .setItem at login

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,12 +1,9 @@
 // src/app/user-login-form/user-login-form.component.ts
 import { Component, OnInit, Input } from '@angular/core';
-
 // You'll use this import to close the dialog on success
 import { MatDialogRef } from '@angular/material/dialog';
-
 // This import brings in the API calls we created in 6.2
-import { UserLoginService } from '../fetch-api-data.service';
-
+import { FetchApiDataService } from '../fetch-api-data.service';
 // This import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -28,7 +25,7 @@ export class UserLoginFormComponent implements OnInit {
   };
 
   constructor(
-    public fetchApiData: UserLoginService,
+    public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserLoginFormComponent>,
     public snackBar: MatSnackBar
   ) { }
@@ -42,6 +39,8 @@ export class UserLoginFormComponent implements OnInit {
     this.fetchApiData.userLogin(this.userData).subscribe((result) => {
       // Logic for a successful user login goes here! (To be implemented)
       this.dialogRef.close(); // This will close the modal on success!
+      localStorage.setItem('user', result.user.Username);
+      localStorage.setItem('token', result.token);
       console.log(result);
       this.snackBar.open(result, 'OK', {
         duration: 2000
