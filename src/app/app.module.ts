89ed9501e7ca8fc0d4bfb27350805f55a34d3ad5
berit-stange <76659950+berit-stange@users.noreply.github.com>
2021-08-06 15:44:20 +0200

    added RouterModule + WelcomePageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
+// "app.module.ts" file is the entry point of your Angular app, 
+// meaning that it's mostly used to wire up different modules together 
+// and express dependencies
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-// simplified API for Angular applications that makes it possible for the client app to communicate with the API or server-side
+// simplified API for Angular applications that makes it possible for the 
+// client app to communicate with the API or server-side
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -13,22 +19,34 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
+
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+//defining a welcome route that'll point to components
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginFormComponent,
-    MovieCardComponent
+    MovieCardComponent,
+    WelcomePageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
     HttpClientModule,
     BrowserAnimationsModule,
 
